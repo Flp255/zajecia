861ea8cefe2ff54336cc484039f87158123cc946
Flp255 <255414@edu.p.lodz.pl>
2025-05-05 16:11:54 +0200

    All work

--- 05.05kalkulator/main.cpp
+++ 05.05kalkulator/main.cpp
@@ -17,6 +17,32 @@ int main(int argc, char const* argv[]) {
     const char* operation = argv[2];
     double b = atof(argv[3]);
     double result;
-
-        return 0;
+    if (strcmp(operation, "add") == 0) {
+        result = a + b;
+        printf("Result: %f + %f =%f\n", a, b, result);
     }
+    else if (strcmp(operation, "sub") == 0) {
+        result = a - b;
+        printf("Result: %f - %f =%f\n", a, b, result);
+    }
+    else if (strcmp(operation, "mul") == 0) {
+        result = a * b;
+        printf("Result: %f * %f =%f\n", a, b, result);
+    }
+    else if (strcmp(operation, "div") == 0) {
+        if (b == 0) {
+            printf("Error: division by zero!\n");
+            return 1;
+        }
+        result = a / b;
+        printf("Result: %f / %f =%f\n", a, b, result);
+    }
+    else {
+        printf("Unknown operation: %s\n", operation);
+        usage(argv[0]);
+    }
+
+
+   
+    return 0;
+}
